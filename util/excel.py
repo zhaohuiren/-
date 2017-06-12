cbf816c351489530005a87f106cfd63c844335fd
ren1995520@gmail.com <rzh110120999>
2017-06-12 18:17:12 +0800

    修改bug

--- util/excel.py
+++ util/excel.py
@@ -1,5 +1,7 @@
 from xlrd import open_workbook
 from xlutils.copy import copy
+import os
+import random
 def receivables(code):
     rb=open_workbook(r'E:\a_测试文件\催收记录导入模板.xls')
     rs=rb.sheet_by_index(0)
@@ -12,6 +14,8 @@ def projects():
     rb=open_workbook(r'E:\a_测试文件\委案\一条.xlsx')
     rs=rb.sheet_by_index(0)
     wb=copy(rb)
+    ws = wb.get_sheet(0)
+    ws.write(2,6,random.randint(0,100))
     wb.save(r'E:\a_测试文件\委案导入模板程序.xls')
 
 
